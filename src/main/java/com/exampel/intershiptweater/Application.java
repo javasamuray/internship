01aac093e360669c9956288df3722eef5e54d4d2
Pavel Matvienko <javasamuray@mail.ru>
2019-04-06 21:20:18 +0700

    добавил панель администратора и роли, но при запуске приложения выходит ошибка - 2019-04-06 21:17:51.204  INFO 22144 --- [nio-8080-exec-3] o.apache.coyote.http11.Http11Processor   : Error parsing HTTP request header  Note: further occurrences of HTTP header parsing errors will be logged at DEBUG level.
    java.lang.IllegalArgumentException: Invalid character found in method name. HTTP method names must be tokens

--- src/main/java/com/exampel/intershiptweater/Application.java
+++ src/main/java/com/exampel/intershiptweater/Application.java
@@ -1,14 +1,8 @@
 package com.exampel.intershiptweater;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
-@Configuration
-@ComponentScan
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/resources/templates/login.ftl
+++ src/main/resources/templates/login.ftl
@@ -1,3 +1,4 @@
+
 <#import "parts/common.ftl" as c>
 <#import "parts/login.ftl" as l>
 
